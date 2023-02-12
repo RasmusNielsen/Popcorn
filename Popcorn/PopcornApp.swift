a190875b36949ca3ba07d8b042b775b24011692e
Rasmus Nielsen <mail@rasmusnielsen.dk>
2023-02-12 17:53:12 +0100

    Initial Commit

---
+++ Popcorn/PopcornApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopcornApp.swift
+//  Popcorn
+//
+//  Created by Rasmus Nielsen on 12/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PopcornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
